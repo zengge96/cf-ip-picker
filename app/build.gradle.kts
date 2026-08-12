3e6e46362b1e3e44c1df10db8280c4d1aa63d740
zengge96 <175998143+zengge96@users.noreply.github.com>
2026-08-12 02:36:01 +0000

    fix: Kotlin 2.0 需要 Compose Compiler Gradle 插件,移除旧的 composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -30,9 +31,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
